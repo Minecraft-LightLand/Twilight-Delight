f57c783da44f7711708bf2e9830d325ca0a34b14
lcy0x1 <chengyilin@umail.ucsb.edu>
2025-05-15 09:35:16 +0800

    drink fix

--- src/main/java/dev/xkmc/twilightdelight/content/item/food/TDDrinkableItem.java
+++ src/main/java/dev/xkmc/twilightdelight/content/item/food/TDDrinkableItem.java
@@ -1,17 +1,23 @@
 package dev.xkmc.twilightdelight.content.item.food;
 
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.UseAnim;
 import vectorwing.farmersdelight.common.Configuration;
-import vectorwing.farmersdelight.common.item.DrinkableItem;
 
 import java.util.List;
 
-public class TDDrinkableItem extends DrinkableItem {
+public class TDDrinkableItem extends Item {
 
 	public TDDrinkableItem(Properties properties) {
 		super(properties);
+	}
+
+	public UseAnim getUseAnimation(ItemStack stack) {
+		return UseAnim.DRINK;
 	}
 
 	@Override
